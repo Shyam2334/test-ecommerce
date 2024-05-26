995f0b84bfbe9a41174bd8599d26994be9152d45
Shyam2334 <shyam.python@gmail.com>
2024-05-26 05:34:40 -0500

    setup user db

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,7 +1,10 @@
 import { createServer } from "./config/app";
+import { connectDB } from "./config/connectdb";
 
 const app=createServer();
 
-app?.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+connectDB().then(()=>{
+    app?.listen(3000,()=>{
+        console.log("Server is running on port 3000");
+    })
 })
